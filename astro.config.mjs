65fd964010718a1811ffb7af21f35aecd363b969
Thomas Beckford <tebeckford@gmail.com>
2025-01-16 21:57:05 -0300

    remove preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-import preact from '@astrojs/preact'
-
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tebeck.vercel.com',
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind()],
 })
